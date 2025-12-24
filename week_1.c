3b7d43652b253cf784e5022139e854a8d3e18036
DongImHan <dongim0712@bu.ac.kr>
2025-12-24 10:27:24 +0900

    commit

---
+++ week_1.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    int x;
+    float y;
+
+    printf("ax^2 + bx + c = y\n");
+
+    printf("Enter a, b, c: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    y = a*x*x +b*x + c;
+
+    printf("The answer is %5.2f\n", y);
+
+    return 0;
+}
