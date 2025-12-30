cb353921c441e90b0bb97ae9a452d17de672b1b3
DongImHan <dongim0712@bu.ac.kr>
2025-12-30 14:06:07 +0900

    2nd commit

---
+++ week_2/ex_continue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int sum = 0;
+
+    for (i = 1; i <= 100; i++)
+    {  
+        if (( i % 3) == 0)
+        {
+            continue;
+        }
+
+    sum += i;
+
+    }
+
+    printf("결과 : %d", sum);
+
+}
