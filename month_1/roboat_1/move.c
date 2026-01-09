4f6660c67c87c585053a2c3a2fc6018e4d4c3456
DongImHan <dongim0712@bu.ac.kr>
2026-01-09 16:40:45 +0900

    2nd commit

--- month_1/roboat_1/move.c
+++ month_1/roboat_1/move.c
@@ -12,6 +12,27 @@ void move(Position *p, int dx, int dy) {
 
 int main(void) {
     Position robot = {0, 0};
+    int n;
+    int dx, dy;
+
+    printf("이동할 횟수를 입력 : ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d번째 이동 (dx, dy) : ", i + 1);
+        scanf("%d %d", &dx, &dy);
+        move(&robot, dx, dy);
+    }
+    printf("로봇 위치 : (%d, %d)", robot.x, robot.y);
+
+    return 0;
+}
+
+
+/*
+int main(void) {
+    Position robot = {0, 0};
 
     move(&robot, +5, +3);
     move(&robot, -2, +4);
@@ -20,3 +41,4 @@ int main(void) {
 
     return 0;
 }
+*/
